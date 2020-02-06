c48dc3d6b0163ca39d7940a8e799658586f7778b
monksedo <chris@tnwebdev.com>
2020-02-06 03:13:07 -0800

    Change default colors

--- bgcolorpicker.js
+++ bgcolorpicker.js
@@ -13,11 +13,13 @@ function changeBgColor() {
   console.log(picker1.value);
   console.log(picker2.value);
   bgColor.style.background = `linear-gradient(to right, ${picker1.value}, ${picker2.value})`;
-  h3Title.textContent = `linear-gradient(to right, ${picker1.value}, ${picker2.value})`;
+  // h3Title.textContent = `linear-gradient(to right, ${picker1.value}, ${picker2.value})`;
+  h3Title.textContent = bgColor.style.background;
 }
 
 // Display default color when page loaded
 h3Title.textContent = `linear-gradient(to right, ${picker1.value}, ${picker2.value})`;
+
 
 // Change first color on color picker confirmed
 picker1.addEventListener('input', changeBgColor);
