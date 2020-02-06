16bf9c9c8d4a9318d824348ebf3e3bd64b6752ec
monksedo <chris@tnwebdev.com>
2020-02-06 03:53:34 -0800

    Load default colors on page loaded

--- bgcolorpicker.js
+++ bgcolorpicker.js
@@ -8,6 +8,9 @@ let picker2 = document.querySelector('.picker2');
 let h3Title = document.querySelector('.h3Title');
 let bgColor = document.querySelector('.bgColor');
 
+console.log(picker1.value);
+console.log(picker2.value);
+
 // Apply change color and update new colors label
 function changeBgColor() {
   console.log(picker1.value);
@@ -16,10 +19,11 @@ function changeBgColor() {
   // h3Title.textContent = `linear-gradient(to right, ${picker1.value}, ${picker2.value})`;
   h3Title.textContent = bgColor.style.background;
 }
+// Set the default color value on page load
+bgColor.style.background = `linear-gradient(to right, ${picker1.value}, ${picker2.value})`;
 
-// Display default color when page loaded
+// Display default colors on page loaded
 h3Title.textContent = `linear-gradient(to right, ${picker1.value}, ${picker2.value})`;
-
 
 // Change first color on color picker confirmed
 picker1.addEventListener('input', changeBgColor);
