f8c56a9cd14d6a0962ea23e58d340e2ffd1d3844
monksedo <chris@tnwebdev.com>
2020-02-06 02:56:05 -0800

    Add comment to js code

--- bgcolorpicker.js
+++ bgcolorpicker.js
@@ -2,11 +2,13 @@
  * Basic color picker exercise
  */
 
+// Select all elements for later use
 let picker1 = document.querySelector('.picker1');
 let picker2 = document.querySelector('.picker2');
 let h3Title = document.querySelector('.h3Title');
 let bgColor = document.querySelector('.bgColor');
 
+// Apply change color and update new colors label
 function changeBgColor() {
   console.log(picker1.value);
   console.log(picker2.value);
@@ -14,10 +16,14 @@ function changeBgColor() {
   h3Title.textContent = `linear-gradient(to right, ${picker1.value}, ${picker2.value})`;
 }
 
+// Display default color when page loaded
+h3Title.textContent = `linear-gradient(to right, ${picker1.value}, ${picker2.value})`;
+
+// Change first color on color picker confirmed
 picker1.addEventListener('input', changeBgColor);
 
+// Change second color on color picker confirmed
 picker2.addEventListener('input', changeBgColor);
 
-h3Title.textContent = `linear-gradient(to right, ${picker1.value}, ${picker2.value})`;
 
 
