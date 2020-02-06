135589626e5db3e27acce66ecf1f9243df6e45ed
monksedo <chris@tnwebdev.com>
2020-02-06 02:41:35 -0800

    Add first version of complete code to repo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# bgcolorpicker
+# Backgound Gradient Color Picker
 A simple background picker app exercise
+
+## Run App
+[Click to run](https://monksedo.github.io/bgcolorpicker/)
---
+++ bgcolorpicker.js
@@ -0,0 +1,21 @@
+/**************************************************************************
+ * Basic color picker exercise
+ */
+
+let picker1 = document.querySelector('.picker1');
+let picker2 = document.querySelector('.picker2');
+let h3Title = document.querySelector('.h3Title');
+let bgColor = document.querySelector('.bgColor');
+
+function changeBgColor() {
+  console.log(picker1.value);
+  console.log(picker2.value);
+  bgColor.style.background = `linear-gradient(to right, ${picker1.value}, ${picker2.value})`;
+  h3Title.textContent = `linear-gradient(to right, ${picker1.value}, ${picker2.value})`;
+}
+
+picker1.addEventListener('input', changeBgColor);
+
+picker2.addEventListener('input', changeBgColor);
+
+
